f7bf6b80599590d35a4ce98a82d34e714c654a90
Adithya <adithyavaradharaj@gmail.com>
2020-02-06 23:34:49 +0530

    src modified

--- Src/Client/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-
-class App extends Component {
-    return <h1>Hello, World</h1>;
-}
-
-render(<App />, document.getElementById('root'));
